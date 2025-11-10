dae32913d83309533d0c488eea1444b9ce5ef0df
Juan-Tapias <jmartineztapias15@gmail.com>
2025-11-10 15:06:29 -0500

    feat: :tada: Se creo la configuracion del backend

---
+++ bodegaslogitrack/src/main/java/com/c3/bodegaslogitrack/BodegaslogitrackApplication.java
@@ -0,0 +1,13 @@
+package com.c3.bodegaslogitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegaslogitrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodegaslogitrackApplication.class, args);
+	}
+
+}
---
+++ bodegaslogitrack/src/test/java/com/c3/bodegaslogitrack/BodegaslogitrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c3.bodegaslogitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegaslogitrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
